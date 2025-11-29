cf1c7a4fd750cc037d16a356ef044af845086f7e
SonaySorokun <sonay.vai@mail.ru>
2025-11-30 02:57:08 +0700

    Цена

--- lab6.py
+++ lab6.py
@@ -4,13 +4,14 @@ from psycopg2.extras import RealDictCursor
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from os import path
+import numpy as np
 
 
 lab6 = Blueprint('lab6', __name__)
 
 offices = []
 for i in range (1, 11):
-    offices.append({'number': i, 'tenant': ''})
+    offices.append({'number': i, 'tenant': '', "price": round(np.random.rand() * 1000)})
 
 @lab6.route('/lab6/')
 def lab():
