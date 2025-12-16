241ea074e43d5edaeaddd1939ae37268c2f62f73
SonaySorokun <sonay.vai@mail.ru>
2025-12-17 02:25:17 +0700

    Авторизация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, abort, jsonify, current_app, redirect
+from flask import Blueprint, render_template, request, abort, jsonify, current_app, redirect,session
 from datetime import datetime
 from werkzeug.security import check_password_hash, generate_password_hash
 import psycopg2
@@ -7,28 +7,30 @@ import sqlite3
 from os import path
 from db import db
 from db.models import users,articles
+from flask_login import login_user, login_required, current_user
 
 lab8 = Blueprint('lab8', __name__)
 
-@lab8.route('/lab8/register', methods = ['GET', 'POST'])
+@lab8.route('/lab8/')
 def lab():
+    return render_template('lab8/lab8.html')
+
+@lab8.route('/lab8/register', methods = ['GET', 'POST'])
+def register():
     if request.method == 'GET':
         return render_template('lab8/register.html')
 
-    login_form = request.form.get('login').strip()
-    password_form = request.form.get('password').strip()
+    login_form = request.form.get('login', '').strip()
+    password_form = request.form.get('password', '').strip()
 
-    errors = []
     
     if not login_form:
-        errors.append('Имя пользователя не может быть пустым')
+        return render_template('lab8/register.html',
+                               error='Имя пользователя не может быть пустым')
     
     if not password_form:
-        errors.append('Пароль не может быть пустым')
-    
-    if errors:
         return render_template('lab8/register.html',
-                               errors=errors)
+                                error='Пароль не может быть пустым')
     
     login_exists = users.query.filter_by(login = login_form).first()
     if login_exists:
@@ -40,3 +42,35 @@ def lab():
     db.session.add(new_user)
     db.session.commit()
     return redirect('/lab8/')
+
+@lab8.route('/lab8/login', methods = ['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab8/login.html')
+    
+    login_form = request.form.get('login', '').strip()
+    password_form = request.form.get('password', '').strip()
+
+    if not login_form:
+        return render_template('lab8/login.html',
+                               error='Логин не может быть пустым')
+    
+    if not password_form:
+        return render_template('lab8/login.html',
+                               error='Пароль не может быть пустым')
+
+
+    user = users.query.filter_by(login = login_form).first()
+
+    if user:
+        if check_password_hash(user.password, password_form):
+            login_user(user, remember= False)
+            return redirect('/lab8/')
+        
+    return render_template('lab8/login.html',
+                               error='Логин и/или пароль неверны')
+
+@lab8.route('/lab8/articles/')
+@login_required
+def article_list():
+    return "список статей"
