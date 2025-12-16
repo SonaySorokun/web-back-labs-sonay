1477159354d2f69fd4b906b80ab8949770508ad2
SonaySorokun <sonay.vai@mail.ru>
2025-12-17 06:36:42 +0700

    лабораторная 9

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 
 app = Flask(__name__)
@@ -56,6 +57,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 
 @app.route("/")
@@ -68,6 +70,7 @@ def title_page():
     lab6_url = url_for("lab6.lab")
     lab7_url = url_for("lab7.lab")
     lab8_url = url_for("lab8.lab")
+    lab8_url = url_for("lab9.lab")
 
 
     return '''
@@ -94,6 +97,7 @@ def title_page():
                 <li><a href="''' + lab6_url + '''">Лабораторная работа #6</a></li>
                 <li><a href="''' + lab7_url + '''">Лабораторная работа #7</a></li>
                 <li><a href="''' + lab8_url + '''">Лабораторная работа #8</a></li>
+                <li><a href="''' + lab9_url + '''">Лабораторная работа #9</a></li>
             </ul>
         </div>
     </main>
